89d6f1506caae6bd2b4c4578accfd7db5f16eff0
aditipandey-prog <aditipandey394@gmail.com>
2023-06-11 12:57:54 +0530

    removed console statements

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let reset=document.getElementById('btn');
 for(list of lists){
     list.addEventListener("dragstart",function(e){ // an eventListener is being added one by one to the fill_list div 
         let selected=e.target;
-        console.log(e.target);
+       
         list.classList.add('dragging');
         emptied.classList.add('bigger');//when the drag starts the drop box will become a bit large to pop out.
         emptied.addEventListener("dragover",function(e){ //
@@ -14,7 +14,7 @@ for(list of lists){
         });
         emptied.addEventListener("drop",function(e){
             e.preventDefault
-            console.log(e.target);
+           
             if(selected!=null){
                 emptied.appendChild(selected);
                 
@@ -29,7 +29,7 @@ for(list of lists){
     });
     //  remove css classes from emptied class and list so that they can go back to their normal size
     list.addEventListener("dragend",function(e){
-        console.log("end");
+      
         emptied.classList.remove('bigger');
         list.classList.remove('dragging');
     })
